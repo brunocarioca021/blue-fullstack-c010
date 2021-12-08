f4bd00f95719635faf01f6a2b97f31aee208c777
Ivo Soares <ivonaldojrsoares@gmail.com>
2021-12-08 20:56:24 -0300

    adiciona backend mvc mongo loja games

---
+++ aula-08-blue-loja-games/backend/index.js
@@ -0,0 +1,29 @@
+// importar as bibliotecas/libs necessarias
+const express = require('express');
+const cors = require('cors');
+// importo o arquivo de conexao para poder acessar a funcao.
+const Conn = require('./conn/conn');
+
+// inicializar o express
+const app = express();
+
+//declaro os middlewares
+app.use(express.json());
+app.use(cors());
+
+// chamo a conexao com o banco de dados
+Conn();
+
+// defino a porta e iniclizo o servidor
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Servidor inicializado na porta ${port}`);
+})
+
+
+// MVC
+// MODEL - É O RESPONSAVEL PELOS DADOS E FUNCOES DO BANCO DE DADOS
+// VIEW - É A VISAO DO CLIENNTE OU SEJA (HTML, APP, SMARTWATCH, SMART TV, VUE, REACT, ANGULAR)
+// CONTROLLER - É RESPONSAVEL POR GERENCIAR OS FLUXOS E DAS REGRAS DO SISTEMA.
+// ROTAS - SAO OS RESPONSAVEIS PELOS ENDPOINTS E METODOS DA API (GET/POST/PUT/DELETE)
+// SERVICOS - RESPONSAVEIS POR SE CONECTAR AO NOSSO MODEL PARA CHAMAR AS FUNCOES DO BANCO DE DADOS (LIDA COM DADOS)
---
+++ aula-08-blue-loja-games/backend/routes/games.routes.js
@@ -0,0 +1,12 @@
+// importar o express
+const express = require('express');
+
+// inicializar o modulo de rotas
+const router = express.Router();
+
+//[ENDPOINTS]
+
+
+
+//exportamos o modulo de rotas
+module.exports = router;
